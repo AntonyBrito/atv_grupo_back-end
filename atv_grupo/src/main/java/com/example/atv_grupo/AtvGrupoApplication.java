bfa73aaa3e8083634f5eb49ed1eb0de5c7f28090
antony <antony.brito047@hotmail.com>
2025-04-24 14:18:15 -0300

    Estrutura inicial back-end

---
+++ atv_grupo/src/main/java/com/example/atv_grupo/AtvGrupoApplication.java
@@ -0,0 +1,13 @@
+package com.example.atv_grupo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvGrupoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvGrupoApplication.class, args);
+	}
+
+}
---
+++ atv_grupo/src/test/java/com/example/atv_grupo/AtvGrupoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atv_grupo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvGrupoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
